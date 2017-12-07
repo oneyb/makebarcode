1e5c63673945cc558bdeaf1f2bb9de324e24be50
oneyb <zenlines@gmail.com>
2017-12-08 00:33:53 +0100

    added show_text option to label sheet creator

--- generate_barcode_simple.py
+++ generate_barcode_simple.py
@@ -10,7 +10,7 @@ from reportlab.pdfgen import canvas
 class BarcodeCanvas(canvas.Canvas):
 
     def draw_labels(self, codes, encoder, label_height, label_width,
-                    no_labels_x, no_labels_y, x_gap, x_offset, y_gap, y_offset): 
+                    no_labels_x, no_labels_y, x_gap, x_offset, y_gap, y_offset, show_text=1): 
         """Draws barcode labels on the Canvas-based BarcodeCanvas class"""
 
         from reportlab.lib.styles import getSampleStyleSheet
@@ -18,20 +18,31 @@ class BarcodeCanvas(canvas.Canvas):
         from reportlab.graphics import barcode
 
 
-        style = getSampleStyleSheet()
-        # center text and wrap if necessary
-        style['Normal'].alignment = 1
+        if show_text==0:
+            height = 0
+        else:
+            from reportlab.platypus import Paragraph
+            from reportlab.lib.styles import getSampleStyleSheet
+            style = getSampleStyleSheet()
+            # center text and wrap if necessary
+            style['Normal'].alignment = 1
+
+
         for i, code in enumerate(codes):
             if i % no_labels_x == 0:
                 x = x_offset
+
             y = y_offset + i / no_labels_x * label_height + i / no_labels_x * y_gap
-            p = Paragraph(code, style=style["Normal"])
-            width, height = p.wrapOn(self, label_height * 0.3, label_width)
-            # self.drawCentredString(x + label_width / 2, y + label_height - height, code)
-            p.drawOn(self, x, y + label_height - height)
+
+            if show_text!=0:
+                p = Paragraph(code, style=style["Normal"])
+                width, height = p.wrapOn(self, label_width, label_height * 0.3)
+                # self.drawCentredString(x + label_width / 2, y + label_height - height, code)
+                p.drawOn(self, x, y + label_height - height)
+
             encoded = barcode.createBarcodeDrawing(encoder,
                                                    **dict(value=code,
-                                                          height=(label_height - height) * 0.88,
+                                                          height=(label_height - height) * 0.98,
                                                           width=label_width))
             # import pdb; pdb.set_trace() 
             encoded.drawOn(self, x, y)
@@ -45,14 +56,13 @@ def main():
     import importlib
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--codes',        default='0001')
-    parser.add_argument('-f', '--filename',     default='barcodes')
-    parser.add_argument('-r', '--recipe',       default='avery_3475')
-    parser.add_argument('-e', '--encoder',      default='Code128')
-    parser.add_argument('-le', '--list-encoders', dest='le', 
-                        action='store_true', default=False)
-    parser.add_argument('-lr', '--list-recipes', dest='lr', 
-                        action='store_true', default=False)
+    parser.add_argument('-c',  '--codes',         default='0001')
+    parser.add_argument('-f',  '--filename',      default='barcodes')
+    parser.add_argument('-r',  '--recipe',        default='avery_3475')
+    parser.add_argument('-e',  '--encoder',       default='Code128', help='Encoder to be used. E.g. QR')
+    parser.add_argument('-s',  '--show_text',     default=1,         type=int)
+    parser.add_argument('-le', '--list-encoders', default=False,     dest='le')
+    parser.add_argument('-lr', '--list-recipes',  default=False,     dest='lr')
     args = parser.parse_args()
 
     if args.le:
@@ -113,7 +123,7 @@ def main():
                         ,bottomup = 0
     )
      
-    pdf.draw_labels(codes, args.encoder, **recipe)
+    pdf.draw_labels(codes, args.encoder, show_text=args.show_text, **recipe)
     pdf.showPage()
     pdf.save()
 
